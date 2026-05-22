b8d17fd1b6524352eac1009136648955148f7813
patelbandilokesh <patelbandilokesh@gmail.com>
2026-05-22 15:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2038,4 +2038,4 @@ def logout():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)),debug=True)
